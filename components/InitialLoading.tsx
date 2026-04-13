320f56bef66daf4fdc355ee625a0e7422cb030d4
Sayan Manna <sayan2003.dev@gmail.com>
2026-04-13 14:12:18 +0000

    feat:Initial Loading Screen

---
+++ components/InitialLoading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InitialLoadingScreen = () => {
+  return (
+    <div className='flex w-screen h-screen bg-black text-white items-center justify-center poppins-extrabold text-7xl'>Orbit</div>
+  )
+}
+
+export default InitialLoadingScreen;
